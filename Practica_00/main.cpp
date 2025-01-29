aa911e69e422f83fb5f835d0241d1f74561d80d7
garriagus <agustin_g08@hotmail.com>
2025-01-29 11:40:17 -0300

    a

--- Practica_00/main.cpp
+++ Practica_00/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
-  std::cout << "esto andaaa" << std::endl;
+	int arr[] = { 12, 11, 13, 5, 6 }; 
+
+	std::cout << "esto es una prueba" << arr << std::endl;
     return 0;
 } 
---
+++ Practica_02/merge_sort.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+	int arr[] = { 12, 11, 13, 5, 6 };  // Arreglo desordenado
+
+	std::cout << "esto andaaa" << arr << std::endl;
+	return 0;
+}
